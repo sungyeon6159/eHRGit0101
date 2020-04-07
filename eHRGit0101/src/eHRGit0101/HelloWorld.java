6d5498c1ada528474fd50ef6922d927932d395f9
sungyeon6159 <53823291+sungyeon6159@users.noreply.github.com>
2020-04-07 09:51:40 +0900

    //첫커밋: 커밋하면 로컬까지 올라가고   커밋and푸쉬: 원격서버까지 올라간다.

---
+++ eHRGit0101/src/eHRGit0101/HelloWorld.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package eHRGit0101;
+
+import java.util.logging.Logger;
+
+/**
+ * @author SIST129
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 *Method Name:main
+	 *작성일: 2020. 4. 7.
+	 *작성자: SIST129
+	 *설명:
+	 *@param args
+	 */
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, Git~!");
+		
+	
+	}
+
+}
